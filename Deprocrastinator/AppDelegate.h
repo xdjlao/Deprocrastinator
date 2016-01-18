2704689b3243d984fe0ce41d1240e17224c520b6
xdjlao <jerry@flatie.com>
2016-01-18 13:33:31 -0800

    initial commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by Jerry on 1/18/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Jerry on 1/18/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
